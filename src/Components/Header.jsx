e3c9aa90c5379d12f267bd924b95076bfb1c15a7
Akkilesh-A <akkilalagar05@gmail.com>
2024-03-18 22:39:59 +0530

    theme,colors changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='z-10 flex flex-col fixed text-xs '>
-      <div className='fixed w-[100vw] sm:h-[8.5vh] h-[15vh] pr-8 pl-8 b-0 flex bg-black text-white font-bold'>
+    <div className=' z-10 flex flex-col fixed text-xs '>
+      <div className='header-bg fixed w-[100vw] sm:h-[8.5vh] h-[15vh] pr-8 pl-8 b-0 flex bg-black text-white font-bold'>
           <div className='flex items-center w-[15vw] font-'>
               <img src='\assets\logo.png' alt='logo' className=' h-[80%] ' />
               <h1 className='text-[1.5em] '>HumanoidX</h1>
--- src/Components/Section-1/LandingContent.jsx
+++ src/Components/Section-1/LandingContent.jsx
@@ -2,9 +2,9 @@ import React,{useState} from 'react'
 
 const LandingContent = () => {
   return (
-    <div className=' pt-[20vh] sm:flex-col flex sm:pb-0 pb-[20vh] text-white badges'>
+    <div className='pt-[20vh] sm:flex-col flex sm:pb-0 pb-[20vh] text-white badges'>
       <div className='px-[8vw] flex flex-col w-[80vw] sm:w-[90vw] pt-[8vh] sm:pt-[0] pb-[20vh] z-0 justify-center'>
-        <h1 className='bg-gradient-to-l from-[#178376] to-black bg-clip-text text-transparent text-[4em] pb-6 font-bold'>Build, Integrate, Innovate</h1>
+        <h1 className='text-[#178376] bg-clip-text  text-[4em] pb-6 font-bold'>Build, Integrate, Innovate</h1>
         <h3 className='text-white text-3xl font-semibold'>Shaping Tomorrrow, Today!</h3>
       </div>
       <div className='mxl:pt-24 mxl:pr-24 flex items-end  sm:justify-center sm:items-center animate-bounce '> 
