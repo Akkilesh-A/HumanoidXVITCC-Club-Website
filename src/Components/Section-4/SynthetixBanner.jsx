d1e69b37f0dd8751b5ba881ff0dbe42ba510910f
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-23 17:31:17 +0530

    button color changed

--- src/Components/Section-4/SynthetixBanner.jsx
+++ src/Components/Section-4/SynthetixBanner.jsx
@@ -14,9 +14,9 @@ const SynthetixBanner = () => {
                     <h1 className='mxl:text-[4em] text-[3em] sm:text-[2em] font-bold text-white'>SYNTHETIX'24</h1>
                     <h4 className='text-black text-[em] mxl:text-[1.5em]  font-semibold'>THE FUTURE IS HERE!</h4>
                 </div>
-                <div className='flex pb-8 justify-center items-end'>
-                    <button className='NavBar-bg hover:bg-white bg-black flex items-center rounded p-2'>
-                        <p className='text-white hover:text-[#178376] px-4 py-2'>Register</p>
+                <div className='flex pb-8 justify-center hover:text-[#178376] items-end'>
+                    <button className='NavBar-bg hover:bg-[#808080]  bg-black flex items-center rounded p-2'>
+                        <p className='text-white  px-4 py-2'>Register</p>
                         <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15 13.5V9L21 15L15 21V16.5H9V13.5H15ZM15 0C23.28 0 30 6.72 30 15C30 23.28 23.28 30 15 30C6.72 30 0 23.28 0 15C0 6.72 6.72 0 15 0ZM15 27C21.63 27 27 21.63 27 15C27 8.37 21.63 3 15 3C8.37 3 3 8.37 3 15C3 21.63 8.37 27 15 27Z" fill="#178376"/>
                         </svg>
