0be4ba9cc10c0f9ced0fc9b4994a4a6150606984
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-23 17:24:33 +0530

    ' added for yers

--- src/Components/Section-3/ImageSlider.jsx
+++ src/Components/Section-3/ImageSlider.jsx
@@ -7,9 +7,9 @@ const ImageSlides = () => {
     const [current, setCurrent] = useState(0);
 
     const pictures=[
-        {id:1,src:'assets/Events/Synthetix23/1.jpg',text:'Synthetix 23'},
-        {id:2,src:'assets/Events/Synthetix23/2.jpg',text:'Synthetix 23'},
-        {id:3,src:'assets/Events/Synthetix23/3.jpg',text:"Synthetix 23"},
+        {id:1,src:'assets/Events/Synthetix23/1.jpg',text:"Synthetix'23"},
+        {id:2,src:'assets/Events/Synthetix23/2.jpg',text:"Synthetix'23"},
+        {id:3,src:'assets/Events/Synthetix23/3.jpg',text:"Synthetix'23"},
         {id:4,src:'assets/Events/NSS/1.jpg',text:'NSS'}
     ]
     function func_right(){
