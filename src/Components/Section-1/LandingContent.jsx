37b558f50c3efdd8aed47d6d08f4e550bc24f7c2
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-23 20:24:46 +0530

    sm text size fixed

--- src/Components/Section-1/LandingContent.jsx
+++ src/Components/Section-1/LandingContent.jsx
@@ -4,7 +4,7 @@ const LandingContent = () => {
   return (
     <div className='pt-[20vh] sm:flex-col sm:pt-[10vh] flex sm:pb-0 pb-[20vh] text-white badges'>
       <div className='px-[8vw] flex flex-col w-[80vw] sm:w-[90vw] pt-[8vh] sm:pt-[0] pb-[20vh] z-0 justify-center'>
-        <h1 className='text-[#178376] bg-clip-text  text-[6em] pb-6 font-bold'>Build, Integrate, Innovate</h1>
+        <h1 className='text-[#178376] bg-clip-text sm:text-[4em] text-[6em] pb-6 font-bold'>Build, Integrate, Innovate</h1>
         <h3 className='text-white text-3xl font-semibold'>Shaping Tomorrrow, Today!</h3>
       </div>
       <div className='mxl:pt-24 mxl:pr-24 flex items-end  sm:justify-center sm:items-center animate-bounce '> 
