fe8c7fb5581a2e02f7fd58355ccaf0b12672d99a
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-23 22:18:16 +0530

    hover effects added

--- src/Components/Section-2/About.jsx
+++ src/Components/Section-2/About.jsx
@@ -55,20 +55,20 @@ const WhoWeAre = () => {
       {/* <hr className='ml-[32vw] w-[60vw] my-[10vh]'/>  */}
 
       <div className='flex sm:flex-col ml-[8vw] w-[84vw] h-[50vh] justify-around items-center'>
-        <div className='sm:order-1 flex w-[30vw] sm:w-[60vw] items-center justify-around '>
-          <img src='assets/Departments/dept-1.png' alt='Departments' className={isDesign?'duration-500 scale-125 w-[6vw] sm:w-[10vw] h-auto':'hover:scale-125 duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' }/>
-          <img src='assets/Departments/dept-2.png' alt='Departments' className={isElectrical?'duration-500 scale-125 w-[6vw] sm:w-[10vw] h-auto':'hover:scale-125 duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />
-          <img src='assets/Departments/dept-3.png' alt='Departments' className={isManagement?'duration-500 scale-125 w-[6vw] sm:w-[10vw] h-auto':'hover:scale-125 duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />
-          <img src='assets/Departments/dept-3.png' alt='Departments' className={isSocialMedia?'duration-500 scale-125 w-[6vw] sm:w-[10vw] h-auto':'hover:scale-125 duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />                
+        <div className='sm:order-1 flex w-[35vw] sm:w-[60vw] items-center justify-between '>
+          <img onMouseOver={setDesignTrue} onMouseOut={setDesignFalse} src='assets/Departments/design&automation.png' alt='Departments' className={isDesign?'duration-500 scale-[2] w-[6vw] sm:w-[10vw] h-auto':'hover:scale-[2]  scale-[1.5] duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' }/>
+          <img onMouseOver={setElectricalTrue} onMouseOut={setElectricalFalse} src='assets/Departments/electrical&programming.png' alt='Departments' className={isElectrical?'duration-500 scale-[2] w-[6vw] sm:w-[10vw] h-auto':'hover:scale-[2]  scale-[1.5] duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />
+          <img onMouseOver={setManagementTrue} onMouseOut={setManagementFalse} src='assets/Departments/management.png' alt='Departments' className={isManagement?'duration-500 scale-[2] w-[6vw] sm:w-[10vw] h-auto':'hover:scale-[2]  scale-[1.5] duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />
+          <img onMouseOver={setSocialMediaTrue} onMouseOut={setMediaFalse} src='assets/Departments/socialmedia.png' alt='Departments' className={isSocialMedia?'duration-500 scale-[2] w-[6vw] sm:w-[10vw] h-auto':'hover:scale-[2] scale-[1.5]  duration-500 hover:duration-500  w-[6vw] sm:w-[10vw] h-auto' } />                
         </div>
         <div className='flex flex-col pt-[5vh] z-0 text-white sm:mb-16 '>
           <h1 className='text-[#178376] text-5xl pb-4 font-bold'>What do we have?</h1>
           <div className='pl-8'>
             <h3 className='text-white text-2xl  font-semibold'>We oversee 4 departments </h3> 
-            <h3 onMouseOver={setDesignTrue} onMouseOut={setDesignFalse} className='hover:scale-125 duration-500 hover:duration-500 text-2xl'>Design & Automation</h3>
-            <h3 onMouseOver={setElectricalTrue} onMouseOut={setElectricalFalse} className='hover:scale-125 duration-500 hover:duration-500 text-white text-2xl '>Electrical & Programming</h3>
-            <h3 onMouseOver={setManagementTrue} onMouseOut={setManagementFalse} className='hover:scale-125 duration-500 hover:duration-500 text-2xl '>Management</h3>
-            <h3 onMouseOver={setSocialMediaTrue} onMouseOut={setMediaFalse} className='hover:scale-125 duration-500 hover:duration-500 text-white text-2xl '>Social Media & Content</h3>
+            <h3 onMouseOver={setDesignTrue} onMouseOut={setDesignFalse} className={isDesign?'scale-125 duration-500 text-2xl':'hover:scale-125 duration-500 hover:duration-500 text-2xl'}>Design & Automation</h3>
+            <h3 onMouseOver={setElectricalTrue} onMouseOut={setElectricalFalse} className={isElectrical?'scale-125 duration-500 text-2xl':'hover:scale-125 duration-500 hover:duration-500 text-2xl'}>Electrical & Programming</h3>
+            <h3 onMouseOver={setManagementTrue} onMouseOut={setManagementFalse} className={isManagement?'scale-125 duration-500 text-2xl':'hover:scale-125 duration-500 hover:duration-500 text-2xl'}>Management</h3>
+            <h3 onMouseOver={setSocialMediaTrue} onMouseOut={setMediaFalse} className={isSocialMedia?'scale-125 duration-500 text-2xl':'hover:scale-125 duration-500 hover:duration-500 text-2xl'}>Social Media & Content</h3>
           </div>       
         </div>
       </div> 
