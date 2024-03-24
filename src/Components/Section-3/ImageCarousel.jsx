1df3dd1ea0bee66ec986895789bff79f8dfbb586
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-24 13:46:49 +0530

    no change when hovered added

--- src/Components/Section-3/ImageCarousel.jsx
+++ src/Components/Section-3/ImageCarousel.jsx
@@ -5,13 +5,15 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const ImageCarousel = () => {
     const [current, setCurrent] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        const id = setInterval(() => {
             setCurrent(current => (current === pictures.length - 1 ? 0 : current + 1));
         }, 3000);
+        setIntervalId(id);
 
-        return () => clearInterval(interval);
+        return () => clearInterval(id);
     }, [pictures.length]);
 
     const goToNext = () => {
@@ -22,12 +24,23 @@ const ImageCarousel = () => {
         setCurrent(current === 0 ? pictures.length - 1 : current - 1);
     };
 
+    const handleMouseEnter = () => {
+        clearInterval(intervalId);
+    };
+
+    const handleMouseLeave = () => {
+        const id = setInterval(() => {
+            setCurrent(current => (current === pictures.length - 1 ? 0 : current + 1));
+        }, 3000);
+        setIntervalId(id);
+    };
+
     return (
         <div id="achievements" className='mb-[10vh]'>
             <SectionHeading text1="Our" text2="Events & Achievements" ptext="Our Events over the years!" />
             <div className='flex justify-center items-center'>
-                <FaArrowLeft className="text-white cursor-pointer  mb-16 mr-8 text-3xl" onClick={goToPrevious} />
-                <div className='flex flex-col items-center text-white relative'>
+                <FaArrowLeft className="text-white cursor-pointer mb-16 mr-8 text-3xl" onClick={goToPrevious} />
+                <div className='flex flex-col items-center text-white relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <img
                         src={pictures[current].src}
                         alt={pictures[current].text}
