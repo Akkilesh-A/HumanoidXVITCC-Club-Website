54f8e9cb97f3bcb7279691896c8d9af51f92876a
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-24 11:21:22 +0530

    photos added to image carousel

--- src/Components/Section-3/picturesImageCarousel.jsx
+++ src/Components/Section-3/picturesImageCarousel.jsx
@@ -2,7 +2,12 @@ const pictures = [
     { id: 1, src: 'assets/Events/Synthetix23/1.jpg', text: "Synthetix'23" },
     { id: 2, src: 'assets/Events/Synthetix23/2.jpg', text: "Synthetix'23" },
     { id: 3, src: 'assets/Events/Synthetix23/3.jpg', text: "Synthetix'23" },
-    { id: 4, src: 'assets/Events/NSS/1.jpg', text: 'NSS' }
+    { id: 4, src: 'assets/Events/NSS/1.jpg', text: "NSS'23" },
+    { id: 5, src: 'assets/Events/NSS24/1.jpg', text: "NSS'24" },
+    { id: 6, src: 'assets/Events/NSS24/2.jpg', text: "NSS'24" },
+    { id: 7, src: 'assets/Events/NSS24/3.jpg', text: "NSS'24" },
+    { id: 8, src: 'assets/Events/NSS24/4.jpg', text: "NSS'24" },
+    { id: 9, src: 'assets/Events/NSS24/5.jpg', text: "NSS'24" },
 ];
 
 export default pictures;
