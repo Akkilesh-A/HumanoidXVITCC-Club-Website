95e14a1eebaf4864a3edcaf67a9b51cb18bdac50
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-25 17:29:35 +0530

    animation added

--- src/Components/Section-1/LandingContent.jsx
+++ src/Components/Section-1/LandingContent.jsx
@@ -7,7 +7,7 @@ const LandingContent = () => {
         <h1 className='text-[#178376] bg-clip-text sm:text-[4em] text-[6em] pb-6 font-bold'>Build, Integrate, Innovate</h1>
         <h3 className='text-white text-[2.5em] font-semibold'>Shaping Tomorrrow, Today!</h3>
       </div>
-      <div className='mxl:pt-24 mxl:pr-24 flex items-end  sm:justify-center sm:items-center animate-bounce '> 
+      <div className='mxl:pt-24 mxl:pr-24 pt-24 flex items-center animate-bouncing  sm:justify-center sm:items-center  '> 
         <img src='assets/robot.png' alt='robot' className='' />
       </div>
     </div>
