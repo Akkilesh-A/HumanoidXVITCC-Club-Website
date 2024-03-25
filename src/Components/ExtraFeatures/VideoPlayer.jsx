3786bf059dcbfab5d4453d1a57b2fad83c5771e7
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-25 14:32:19 +0530

    video settings updated

--- src/Components/ExtraFeatures/VideoPlayer.jsx
+++ src/Components/ExtraFeatures/VideoPlayer.jsx
@@ -7,7 +7,7 @@ const VideoPlayer = () => {
         <SectionHeading text1="Our" text2="Projects" ptext="" />
         <div className='flex justify-center items-center w-[100%] sm:w-[80%]  flex-wrap'>
             <iframe width="420" height="315"
-              src="https://www.youtube.com/embed/U2KpRZCoysc?autoplay=1&mute=1">
+              src="https://www.youtube.com/embed/U2KpRZCoysc?autoplay=1&mute=1&loop=1&controls=0">
             </iframe>
         </div>
         <hr className='ml-[8vw] w-[84vw] mt-[10vh]'/> 
