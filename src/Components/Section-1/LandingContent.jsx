9b8f8013fb14ff9f1c2b0f2c54341fd3c8a35033
SPACECENTRE <akkilalagar05@gmail.com>
2024-03-26 11:09:33 +0530

    padding updated

--- src/Components/Section-1/LandingContent.jsx
+++ src/Components/Section-1/LandingContent.jsx
@@ -8,8 +8,8 @@ const LandingContent = () => {
             <source src="assets/reel.mp4" type="video/mp4" />
           </video>
       </div>
-      <div className='flex sm:flex-col'>
-        <div className='px-[8vw] flex flex-col w-[80vw] sm:w-[90vw] pt-[8vh] sm:pt-[0] pb-[20vh] sm:pb-8 z-0 justify-center'>
+      <div className='sm:pb-[20vh] flex sm:flex-col'>
+        <div className='px-[8vw] flex flex-col w-[80vw] sm:w-[90vw] pt-[8vh] sm:pt-[0] sm:pb-8 z-0 justify-center'>
         <h1 className='text-[#178376] bg-clip-text sm:text-[4em] text-[6em] pb-6 font-bold'>Build | Integrate | Innovate |</h1>
         <h3 className='text-white text-[2.5em] font-semibold'>Shaping Tomorrrow, Today!</h3>
         </div>
