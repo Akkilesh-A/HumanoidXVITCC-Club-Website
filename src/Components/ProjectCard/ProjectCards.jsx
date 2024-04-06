2a09ae038aa39215401ee8082c93c44954816bec
SPACECENTRE <akkilalagar05@gmail.com>
2024-04-06 22:23:30 +0530

    project update

--- src/Components/ProjectCard/ProjectCards.jsx
+++ src/Components/ProjectCard/ProjectCards.jsx
@@ -3,15 +3,15 @@ import SectionHeading from '../ExtraFeatures/SectionHeading'
 
 const projects=[
     {imgURL: "assets/Events/Flipkart/3.jpeg", title: 'Drone', description: 'Drone that helped us reach finals of Flipkart GRID 4.0 Robotics Competition'},
-    {imgURL: "assets/Events/TechnoVIT/1.jpg", title: 'Project 1', description: 'This is a description of project 1'},
-    {imgURL: "assets/Events/TechnoVIT/2.jpg", title: 'Project 2', description: 'This is a description of project 2'},
+    {imgURL: "assets/Projects/self_balancing.jpg", title: 'Self Balancing Bot', description: 'A bot that can balance itself on two wheels.'},
+    // {imgURL: "assets/Events/TechnoVIT/2.jpg", title: 'Project 2', description: 'This is a description of project 2'},
 ]
 
 function createProjectCard(project){
     return (
         <div className='hover:scale-110 duration-500 hover:duration-500 m-4 sm:m-8 flex justify-center items-center text-white flex-col '>
             <img loading="lazy" className='w-[18vw] sm:w-[100%] rounded-t-xl' src={project.imgURL} alt={project.title}/>
-            <div className='flex flex-col items-center sm:w-[100%] w-[18vw] rounded-b-xl bg-[#178376] p-4'>
+            <div className='text-center flex flex-col items-center sm:w-[100%] w-[18vw] rounded-b-xl bg-[#178376] p-4'>
                 <h1 className='text-[1.5rem] font-bold'>{project.title}</h1>
                 <p className='text-center text-black font-semibold pt-4'>{project.description}</p> 
             </div>  
